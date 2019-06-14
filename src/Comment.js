9b37217efa94d97ae83dfa28f7390db91b4cf8d1
3014zhangshuo <3014zhangshuo@gmail.com>
2019-06-14 12:28:31 +0800

    显示评论发布时间

--- src/Comment.js
+++ src/Comment.js
@@ -1,6 +1,35 @@
 import React, { Component } from "react"
 
 class Comment extends Component {
+  constructor() {
+    super()
+    this.state = { timeString: '' }
+  }
+
+  componentWillMount() {
+    this._updateTimeString()
+    this._timer = setInterval(
+      this._updateTimeString.bind(this),
+      5000
+    )
+  }
+
+  componentWillUnmount() {
+    console.log('clearInterval TimeString..')
+    clearInterval(this._timer)
+  }
+
+  _updateTimeString() {
+    console.log('Updating TimeString..')
+    const comment = this.props.comment
+    const duration = (+Date.now() - comment.createdAt) / 1000
+    this.setState({
+      timeString: duration > 60
+        ? `${Math.round(duration / 60)} 分钟前`
+        : `${Math.round(Math.max(duration, 1))} 秒前`
+    })
+  }
+
   render() {
     return(
       <div className="comment">
@@ -8,6 +37,9 @@ class Comment extends Component {
           <span>{this.props.comment.username}：</span>
         </div>
         <p>{this.props.comment.content}</p>
+        <span className="comment-createdtime">
+          {this.state.timeString}
+        </span>
       </div>
     )
   }
--- src/CommentInput.js
+++ src/CommentInput.js
@@ -47,7 +47,11 @@ class CommentInput extends Component {
   handleSubmit() {
     if (this.props.onSubmit) {
       const { username, content } = this.state
-      this.props.onSubmit({username, content})
+      this.props.onSubmit({
+        username: username, 
+        content: content,
+        createdAt: +new Date()
+      })
     }
     this.setState({ content: '' })
   }
